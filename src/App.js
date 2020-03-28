619e131a289967a2ac2f8cbdac247b76d7b7f073
Jati Pikukuh <jati.pikukuh@mail.ugm.ac.id>
2020-03-28 21:22:36 +0700

    change bootstrap to antd

--- src/App.js
+++ src/App.js
@@ -1,45 +1,41 @@
-import React,{useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import {Table} from 'antd';
 import './App.css';
+import 'antd/dist/antd.css';
 
 export default function App() {
-  const [employees,setEmployees] = useState([]);
+    const [employees, setEmployees] = useState([]);
 
-  useEffect(() => {
-    axios.get(`http://localhost:8000/api/employees`)
-      .then(res => {
-        console.log(res.data)
-        setEmployees(res.data)
-      })
-  },[]);
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/employees`)
+            .then(res => {
+                console.log(res.data);
+                setEmployees(res.data)
+            })
+    }, []);
 
-  return (
-    <div className="App">
-      <table className="table table-stripped table-hover">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Job</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Option</th>
-          </tr>
-        </thead>
-        <tbody>
-          {employees.map((data, key) => {
-            return(
-            <tr key={key}>
-              <td>{data.name}</td>
-              <td>{data.id_jobs}</td>
-              <td>{data.email}</td>
-              <td>{data.phone}</td>
-              <td></td>
-            </tr>
-            )
-          })}
-        
-        </tbody>
-      </table>
-    </div>
-  );
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            key: 'email',
+        },
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address',
+        },
+    ];
+
+    return (
+        <div className="App">
+            <Table dataSource={employees} columns={columns}/>;
+        </div>
+    );
 }
